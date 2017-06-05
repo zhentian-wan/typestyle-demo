9e37068cf7e04daea5b07903935783c8b21a8a95
zwan <zhentian.wan@ssh.com>
2017-06-05 13:43:35 +0300

    setup possible to debug with iphone

---
+++ src/02.app-media.tsx
@@ -0,0 +1,34 @@
+import {style, media} from 'typestyle';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const fontSize = (value: number | string) => {
+    const valueStr = typeof value === 'string' ?
+        value :
+        value + 'px';
+    return {
+        fontSize: valueStr
+    }
+};
+const className = style(
+    {
+        color: 'red',
+        transition: 'font-size 0.2s',
+        $nest: {
+            /** iphone */
+            '@media screen and (-webkit-min-device-pixel-ratio: 2)': {
+                backgroundColor: 'blue'
+            }
+        }
+    },
+    media({maxWidth: 700, minWidth: 500}, fontSize(20)),
+    media({minWidth: 701}, fontSize(30)),
+    media({maxWidth: 499}, fontSize(15))
+);
+
+ReactDOM.render(
+    <div className={className}>
+        Hello Typestyle
+    </div>,
+    document.getElementById('root')
+);
