c970919738f43e34e5f616868b0635e3150d365c
Zhentian Wan <answer881215@gmail.com>
2017-06-07 23:20:21 +0300

    fallback

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,32 @@
-import {cssRaw} from 'typestyle';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { style, cssRaw, types } from "typestyle";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 cssRaw(`
     body {
         font-size: 1.5em;
         font-weight: bold;
-        background-color: black;
         color: gold
     }
 `);
 
-const App = () => (
-    <div>
-        Hello World!
-    </div>
+const scroll: types.NestedCSSProperties = {
+  "-webkit-overflow-scrolling": "touch",
+  overflow: "auto"
+};
 
-);
+const bg = style(scroll, {
+  backgroundColor: [
+    /* The fallback */
+    "rgb(200, 54, 54)",
+    /** Graceful upgrade */
+    "rgba(200, 54, 54, 0.5)"
+  ]
+});
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+const App = () =>
+  <div className={bg}>
+    Hello World!
+  </div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
